853ac8221c8b32dfc7d7fef6e503740dc542fda9
alexbazhan <alex_bazhan@mail.ru>
2020-02-06 18:53:36 +0400

    'initial'

---
+++ bolg/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+
+
+def post_list(request):
+	posts = Post.objects.all().order_by('published_date')
+	return render(request, 'bolg/post_list.html', {'posts': posts[(len(posts)-5):]})
+# Create your views here.
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'bolg/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'bolg/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'bolg/post_edit.html', {'form': form})
